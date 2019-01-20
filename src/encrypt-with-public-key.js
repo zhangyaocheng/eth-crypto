ddd3b9a618ccef66254b7d404c3c13a55bae9a08
zhangyaocheng <910486122@qq.com>
2019-01-20 20:45:00 +0800

    Update encrypt-with-public-key.js

--- src/encrypt-with-public-key.js
+++ src/encrypt-with-public-key.js
@@ -12,11 +12,17 @@ export default function encryptWithPublicKey(publicKey, message) {
 
     // re-add the compression-flag
     const pubString = '04' + publicKey;
+    
+    const opts = {
+        ephemPrivateKey:new Buffer("fca39e05a26cde57a09a4ba891afe252707758acd8b332f6fad4c20327bc70b6",'hex'),
+        iv: new Buffer("d100f13bf818aa7441ecc7edb34ebcb4",'hex')
+    }
 
 
     return encrypt(
         new Buffer(pubString, 'hex'),
-        Buffer(message)
+        Buffer(message),
+        opts,
     ).then(encryptedBuffers => {
         const encrypted = {
             iv: encryptedBuffers.iv.toString('hex'),
